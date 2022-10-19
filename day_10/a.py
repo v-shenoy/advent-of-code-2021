89f035c45006b372f7faf697bc9ae6173a086655
Vighnesh Shenoy <vshenoy@microsoft.com>
2022-10-19 17:42:28 +0530

    Solutions for day 10.
    Signed-off-by: Vighnesh Shenoy <vshenoy@microsoft.com>

---
+++ day_10/a.py
@@ -0,0 +1,25 @@
+# We use a stack and keep pushing the opening symbols. 
+# When we hit a closing symbol, we check the symbol on the stack.
+# The string is corrupted, if the symbol on the stack is not
+# the corresponding opening symbol
+import time
+
+
+if __name__ == "__main__":
+    t = time.time()
+    opening = ["(", "{", "[", "<"]
+    points = {")": 3, "]": 57, "}": 1197, ">": 25137}
+    opening_to_closing = {"(": ")", "[": "]", "{": "}", "<": ">"}
+    
+    ans = 0
+    with open("inputs/10.txt") as f:
+        for line in f:
+            stack = []
+            for c in line.strip():
+                if c in opening:
+                    stack.append(c)
+                elif c != opening_to_closing[stack.pop()]:
+                    ans += points[c]
+                    break
+    
+    print(f"Ans - {ans}, Time - {time.time() - t}s")
---
+++ day_10/b.py
@@ -0,0 +1,33 @@
+# We perform the same steps used for checking corruption.
+# Once we know that the string is not corrupted, we start
+# popping off the stack and adding the corresponding closing brace.
+# We put these in a list and take the median.
+import time
+import statistics
+
+
+if __name__ == "__main__":
+    t = time.time()
+    opening = ["(", "{", "[", "<"]
+    points = {")": 1, "]": 2, "}": 3, ">": 4}
+    opening_to_closing = {"(": ")", "[": "]", "{": "}", "<": ">"}
+    
+    scores = []
+    with open("inputs/10.txt") as f:
+        for line in f:
+            stack = []
+            ans = 0
+            is_corrupted = False
+            for c in line.strip():
+                if c in opening:
+                    stack.append(c)
+                elif c != opening_to_closing[stack.pop()]:
+                    is_corrupted = True
+                    break
+        
+            if not is_corrupted:
+                while stack:
+                    ans = 5 * ans + points[opening_to_closing[stack.pop()]]
+                scores.append(ans)
+
+    print(f"Ans - {statistics.median(scores)}, Time - {time.time() - t}s")
